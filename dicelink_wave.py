a883e5bf3196abcf3ba24f1495c445b6f2d6fddd
Klaus Weidner <kweidner@pobox.com>
2009-10-04 16:42:09 -0700

    github export

---
+++ dicelink_wave.py
@@ -0,0 +1,54 @@
+from waveapi import document
+from waveapi import events
+from waveapi import model
+from waveapi import robot
+
+import re
+import roll
+
+VERSION='7'
+
+def OnRobotAdded(properties, context):
+  """Invoked when the robot has been added."""
+  root_wavelet = context.GetRootWavelet()
+  root_wavelet.CreateBlip().GetDocument().SetText('DiceLink v%s joined.' % VERSION)
+
+def SetTextWithAttributes(doc, start, end, texts):
+  old_len = end - start 
+  new_text = ''.join([p[0] for p in texts])
+  doc.SetTextInRange(document.Range(start, end), new_text)
+  for text, anno, val in texts:
+    doc.SetAnnotation(document.Range(start, start+len(text)), anno, val)
+    start += len(text)
+  return len(new_text) - old_len
+
+def OnBlipSubmitted(properties, context):
+  """Invoked when a blip was submitted."""
+  blip = context.GetBlipById(properties['blipId'])
+  creator = blip.GetCreator()
+  doc = blip.GetDocument()
+  txt = doc.GetText()
+  out = []
+  offset = 0
+  for spec in roll.GetRollMatches(txt):
+    num, detail = roll.RollDice(spec)
+    match_start = spec['start'] +offset
+    match_end = spec['end'] + offset
+    offset += SetTextWithAttributes(doc, match_start, match_end, [
+      (spec['spec'], 'style/color', '#aa00ff'),
+      ('=%d' % num, 'style/fontWeight', 'bold'),
+      (' [%s]' % detail, 'style/color', 'gray'),
+    ])
+    #out.append('%s rolled %s: %d [%s]' % (creator, spec['spec'], num, detail))
+  if out:
+    root_wavelet = context.GetRootWavelet()
+    root_wavelet.CreateBlip().GetDocument().SetText('\n'.join(out))
+
+if __name__ == '__main__':
+  myRobot = robot.Robot('dicelink', 
+      image_url='http://dicelink.appspot.com/img/icon.png',
+      version=VERSION,
+      profile_url='http://dicelink.appspot.com/')
+  myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
+  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
+  myRobot.Run()
---
+++ roll.py
@@ -0,0 +1,75 @@
+import re
+import random
+
+# Examples:
+# d6
+# 2d6+10
+# 6d8b2+10
+ROLL_RE = re.compile(r'''
+      (?P<spec>
+        (?P<num> \d+ )?        # {6}d8b2+10, optional
+	d                      # 6{d}8b2+10
+	(?P<sides> \d+ )       # 6d{8}b2+10
+	(?:                    # 6d8{b(2)}+10, optional
+	  b
+	  (?P<brutal> \d+ )
+	)?
+	(?P<delta> [+-]\d+ )?  # 6d8b2{+10}, optional
+      )
+      (?P<output>              # 6d8b2+10{=33 [...]}
+        = \d+
+	(?:
+	  \s*
+	  \[
+	    [^]]*
+	  \]
+	)?
+      )?
+	''', re.X)
+
+def GetRollMatches(txt):
+  out = []
+  for m in ROLL_RE.finditer(txt):
+    dict = m.groupdict(0)
+    dict['start'], dict['end'] = m.span()
+    out.append(dict)
+  return out
+
+MAX_NUM=1000
+
+def RollDice(spec):
+  num = int(spec['num'])
+  if num == 0:
+    num = 1
+  sides = int(spec['sides'])
+  brutal = int(spec['brutal'])
+  delta = int(spec['delta'])
+
+  if num > MAX_NUM:
+    return 0, ('Error: number of dice must be <= %d' % MAX_NUM)
+  if brutal > sides:
+    return 0, 'Error: brutal number must be <= number of sides'
+
+  detail = []
+  result = 0
+  for n in xrange(num):
+    while True:
+      roll = random.randint(1, sides)
+      if roll < brutal:
+        detail.append('(%d)' % roll)
+      else:
+        result += roll
+        detail.append('%d,' % roll)
+	break
+
+  result += delta
+  detail.append('%+d' % delta)
+
+  return result, ''.join(detail)
+
+if __name__ == '__main__':
+  random.seed(42)
+  for spec in GetRollMatches('d6, 2d6+10, 6d8b2+10=33 [foo]'):
+    print repr(spec)
+    result, detail = RollDice(spec)
+    print '%s: %d [%s]' % (spec['spec'], result, detail)
