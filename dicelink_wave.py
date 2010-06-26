4a2bd9821e2040624e206c5e92d47dcbeed7bb5d
Klaus Weidner <kweidner@pobox.com>
2010-06-26 12:18:25 -0700

    Migrate to v2 of the wave robot API (from v1)

--- dicelink_wave.py
+++ dicelink_wave.py
@@ -1,25 +1,21 @@
-import logging
-
 from waveapi import events
 from waveapi import robot
+from waveapi import appengine_robot_runner
+import logging
 
 import config
 import wave_ops
 
-VERSION='17'
-
 if __name__ == '__main__':
   myRobot = robot.Robot(config.getConfig('Name', 'dicelink'),
       image_url=config.getConfig('ImageUrl', 'http://dicelink.appspot.com/img/icon.png'),
-      version=config.getConfig('Version', VERSION),
       profile_url=config.getConfig('ProfileUrl', 'http://dicelink.appspot.com/'))
   logging.getLogger().setLevel(config.getConfig('LogLevel', logging.INFO))
-  myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, wave_ops.OnRobotAdded)
-  myRobot.RegisterHandler(events.BLIP_SUBMITTED, wave_ops.OnBlipSubmitted)
-  myRobot.RegisterHandler(events.BLIP_DELETED, wave_ops.OnBlipDeleted)
-  myRobot.RegisterHandler(events.FORM_BUTTON_CLICKED, wave_ops.OnButtonClicked)
+  myRobot.register_handler(events.WaveletSelfAdded, wave_ops.OnRobotAdded)
+  myRobot.register_handler(events.WaveletBlipRemoved, wave_ops.OnBlipDeleted, events.Context.SELF)
+  myRobot.register_handler(events.FormButtonClicked, wave_ops.OnButtonClicked, events.Context.SELF)
+  myRobot.register_handler(events.BlipSubmitted, wave_ops.OnBlipSubmitted, events.Context.SELF)
 
   # ../secrets/*.py
   #myRobot.setup_oauth(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET, server_rpc_base='http://gmodules.com/api/rpc') 
-
-  myRobot.Run()
+  appengine_robot_runner.run(myRobot)
--- wave_ops.py
+++ wave_ops.py
@@ -1,8 +1,7 @@
 import logging
 import re
 
-from waveapi import document
-from waveapi import model
+from waveapi import element
 from waveapi import ops
 
 import controller
@@ -12,36 +11,25 @@ import dicelink # for canonical_campaign only, FIXME
 
 #GADGET_URL='http://dicelink.appspot.com/static/counter.xml'
 
-def SetTextOfBlip(context, waveId, waveletId, blipId, text):
-  context.builder.DocumentDelete(waveId, waveletId, blipId)
-  context.builder.DocumentInsert(waveId, waveletId, blipId, text)
-
-def SetTextWithAttributes(doc, start, end, texts):
+def SetTextWithAttributes(blip, start, end, texts):
+  logging.debug("start=%s end=%s texts=%s", start, end, texts);
   old_len = end - start 
-  new_text = ''.join([p[0] for p in texts])
-  doc.SetTextInRange(document.Range(start, end), new_text)
-  for lst in texts:
-    len_text = len(lst[0])
-    for anno, val in lst[1:]:
-      doc.SetAnnotation(document.Range(start, start+len_text), anno, val)
-    start += len_text
-  return len(new_text) - old_len
+  if old_len > 0:
+    blip.range(start, end).delete()
+  new_len = 0
+  for lst in reversed(texts):
+    blip.at(start).insert(lst[0], bundled_annotations=lst[1:])
+    new_len += len(lst[0])
+  return new_len - old_len
 
-def OnRobotAdded(properties, context):
+def OnRobotAdded(event, wavelet):
   """Invoked when the robot has been added."""
-  root_wavelet = context.GetRootWavelet()
-  if not root_wavelet:
-    logging.warning("can't create welcome blip, no root_wavelet")
-    logging.info('properties: %s', repr(properties))
-    logging.info('context: %s', repr(context))
+  if not wavelet:
+    logging.warning("can't create welcome blip, no wavelet")
     return
-  blip = root_wavelet.CreateBlip()
-  doc = blip.GetDocument()
-  SetTextWithAttributes(doc, 0, 0, [
-	  ['DiceLink joined. '],
-	  ['Privacy policy, Help', ('link/manual', 'https://wave.google.com/wave/#restored:wave:googlewave.com!w%252BeDRGxAAiN')],
-	  [' '],
-  ])
+  blip = wavelet.reply()
+  blip.append('DiceLink joined. ')
+  blip.append('Privacy policy, Help', [('link/manual', 'https://wave.google.com/wave/#restored:wave:googlewave.com!w%252BeDRGxAAiN')])
   #doc.AppendElement(
   #    document.FormElement(
   #        document.ELEMENT_TYPE.BUTTON, 'test', value='Test!'))
@@ -49,43 +37,33 @@ def OnRobotAdded(properties, context):
   #counter = document.Gadget(GADGET_URL)
   #doc.AppendElement(counter)
 
-def OnButtonClicked(properties, context):
-  logging.info("button clicked:\n%s\n%s", repr(properties), repr(context))
-  blipId = properties['blipId']
-  blip = context.GetBlipById(blipId)
+def OnButtonClicked(event, wavelet):
+  logging.info("button clicked:\n%s\n%s", repr(event), repr(wavelet))
+  blipId = event.blip_id
+  blip = event.blip
   if not blip:
-    logging.warning('Blip "%s" not found in context: %s' % (blipId, repr(context)))
+    logging.warning('Blip "%s" not found in context: %s' % (blipId, repr(event)))
     return
 
-  modifier = properties.get('modifiedBy')
-  button = properties.get('button')
+  modifier = event.modified_by
+  #button = event.button_name
 
   doc = blip.GetDocument()
   txt = doc.GetText()
-  for key, elem in blip.GetElements().iteritems():
-    if elem.type == document.ELEMENT_TYPE.BUTTON:
+  for index, elem in enumerate(blip.elements):
+    if issubclass(elem, element.Button):
       if elem.name == 'test':
-	doc.ReplaceElement(key,
-	  document.FormElement(document.ELEMENT_TYPE.BUTTON, 'Roll', value='Roll!'))
-	doc.InsertElement(key,
-	  document.FormElement(document.ELEMENT_TYPE.INPUT, 'test2', value='Test?'))
+	blip.append(element.Button('Roll', value='Roll!'))
+	blip.append(element.Input('test2', value='Test?'))
       else:
-	doc.ReplaceElement(key,
-	  document.FormElement(document.ELEMENT_TYPE.BUTTON, 'test', value='Test again!'))
-    logging.info("element: %s %s", repr(key), repr(elem))
+	blip.append(element.Button('test', value='Test again!'))
+    logging.info("element: %s %s", repr(index), repr(elem))
 
-def OnBlipDeleted(properties, context):
+def OnBlipDeleted(event, wavelet):
   """Invoked when a blip was deleted."""
-  blipId = properties['blipId']
-  wavelets = context.GetWavelets()
-  if not wavelets:
-    logging.warning('OnBlipDeleted: no wavelets in context')
-    return
-  if len(wavelets) > 1:
-    logging.warning('OnBlipDeleted: more than one wavelet in context')
-    return
-  waveletId = wavelets[0].GetId()
-  waveId = wavelets[0].GetWaveId()
+  blipId = event.blip_id
+  waveletId = wavelet.wavelet_id
+  waveId = wavelet.wave_id
 
   charstore_gae.DeleteCharactersInBlip(waveId, waveletId, blipId)
 
@@ -100,36 +78,35 @@ def bad_anchor(txt):
 
 TRAILING_AT_RE = re.compile('@ \s* $', re.X)
 
-def OnBlipSubmitted(properties, context):
+def OnBlipSubmitted(event, wavelet):
   """Invoked when a blip was submitted."""
-  blipId = properties['blipId']
-  blip = context.GetBlipById(blipId)
+  blipId = event.blip_id
+  blip = event.blip
   if not blip:
     logging.warning('Blip "%s" not found in context: %s' % (blipId, repr(context)))
     return
-  waveId = blip.GetWaveId()
-  waveletId = blip.GetWaveletId()
-  creator = blip.GetCreator()
-  modifier = properties.get('modifiedBy')
+  waveId = wavelet.wave_id
+  waveletId = wavelet.wavelet_id
+  creator = blip.creator
+  modifier = event.modified_by
   if not modifier:
     logging.warning('No "modifiedBy" property available, using creator. FIXME!')
     modifier = creator
-  doc = blip.GetDocument()
-  txt = doc.GetText()
+  txt = blip.text
 
   def replacer(start, end, texts):
-    return SetTextWithAttributes(doc, start, end, texts)
+    return SetTextWithAttributes(blip, start, end, texts)
   def sheetTxt():
     replacements = []
-    for anno in blip.GetAnnotations():
+    for anno in blip.annotations:
       if 'link/' in anno.name:
         link = dicelink.canonical_campaign(anno.value)
         if '!w+' in link:
-	  start, end = controller.fix_anchor(txt, anno.range.start, anno.range.end)
+	  start, end = controller.fix_anchor(txt, anno.start, anno.end)
 	  if start == end:
             logging.info('ignoring malformed anchor text')
             continue
-          logging.debug('found link %s (%d-%d)', repr(link), anno.range.start, anno.range.end)
+          logging.debug('found link %s (%d-%d)', repr(link), anno.start, anno.end)
 	  if TRAILING_AT_RE.search(txt[:start]):
             logging.info('Converting wave link %s to wave ID %s', repr(txt[start:end]), link)
 	    replacements.append((start, end, link))
